88bef0d2deaee339734e46488f83ea970b26ef13
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2020-07-04 17:10:27 +0200

    Add hero-detail-component for SRP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from "./heroes/heroes.component";
 import { FormsModule } from "@angular/forms";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeroesComponent
+    AppComponent, HeroesComponent, HeroDetailComponent
   ],
   imports: [
     BrowserModule,
