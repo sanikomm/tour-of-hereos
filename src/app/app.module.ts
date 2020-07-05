15be0d3c7ab724829b6086f838006d7f0541fe3e
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2020-07-05 18:09:45 +0200

    Add dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { FormsModule } from "@angular/forms";
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent
+    AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent
   ],
   imports: [
     BrowserModule,
