33a34524b096d826cfcb29b853d562bdff290c40
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2020-07-11 07:35:02 +0200

    Add in-app navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,21 +3,29 @@ import {Hero} from "./hero";
 import {HEROES} from "./mock-heroes";
 import { Observable, of } from "rxjs";
 import {MessageService} from "./message.service";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
